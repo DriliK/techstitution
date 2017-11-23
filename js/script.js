99212a866b6c3d3df67b0037cd737241a64946d0
unknown <drilona.kurtalani@gmail.com>
2017-11-23 10:32:06 +0100

    Structure files are added.

---
+++ js/script.js
@@ -0,0 +1,7 @@
+    function myFunction() {
+    alert('Hello');
+}
+function consoleName(name,lastname){
+	console.log("Emri dhe mbiemri im eshte:",name,lastname);
+}
+consoleName("Drilona","Kurtalani");

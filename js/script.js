dd3fe56fca3a385a25933d5a79327cabf64c4cf5
unknown <drilona.kurtalani@gmail.com>
2017-11-24 13:32:48 +0100

    Form style

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
-//    function myFunction() {
-//    alert('Hello');
-//}
-//function consoleName(name,lastname){
-//	console.log("Emri dhe mbiemri im eshte:",name,lastname);
-//}
-//consoleName("Drilona","Kurtalani");
+$(document).ready(function(){
+	console.log("DOM is ready!")
 
+	$('#border-wait-form').submit(function(event){
+		event.preventDefault();
+		//HyrjeDalje-minuta
+		var pikaKufitare = $('#pika-kufitare').val();
+		var hyrjeMinMinuta = $('#hyrje-min-minuta').val();
+		var hyrjeMaxMinuta = $('#hyrje-max-minuta').val();
+		var daljeMinMinuta = $('#dalje-min-minuta').val();
+		var daljeMaxMinuta = $('#dalje-max-minuta').val();
+		//HyrjeDalje-metra
+		var hyrjeMinMetra = $('#hyrje-min-metra').val();
+		var hyrjeMaxMetra = $('#hyrje-max-metra').val();
+		var daljeMinMetra = $('#dalje-min-metra').val();
+		var daljeMaxMetra = $('#dalje-max-metra').val();
+
+		console.log("Pika kufitare: ", pikaKufitare, "Hyrje Min:", hyrjeMinMinuta,"Hyrje Max:", hyrjeMaxMinuta, 
+			"Dalje Min:", daljeMinMinuta,"Dalje max:", daljeMaxMinuta, "Hyrje Min:", hyrjeMinMetra, "Hyrje Max:",
+			 hyrjeMaxMetra, "Dalje Min:", daljeMinMetra, "Dalje Max:", daljeMaxMetra);
+
+	});
+});
